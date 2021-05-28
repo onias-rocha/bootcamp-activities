2e8c4df891b37d112af1206255bed8ca1baaba51
onias da Rocha <onias.rocha@mercadolivre.com>
2021-05-27 21:29:34 -0400

    setting up the environment

---
+++ src/main/java/com/meli/bootcampactivities/BootcampActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.meli.bootcampactivities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampActivitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampActivitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/bootcampactivities/BootcampActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.bootcampactivities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
